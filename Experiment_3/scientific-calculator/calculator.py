6bedfe2a34477774daef40060b11ef2068d5f637
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2026-02-05 11:42:23 +0530

    Add files via upload

---
+++ Experiment_3/scientific-calculator/calculator.py
@@ -0,0 +1,7 @@
+from basic_operations import add, subtract, multiply, divide
+from advanced_operations import square_root, power, sine, cosine, tangent
+
+def evaluate_expression():
+    return add(10, sine(30))
+
+
---
+++ Experiment_3/scientific-calculator/tests/test_basic.py
@@ -0,0 +1,13 @@
+import unittest
+from basic_operations import add, subtract, multiply, divide
+
+class TestBasicOperations(unittest.TestCase):
+    def test_add(Self):
+        self.assertEqual(add(2,3), 5)
+
+    def test_divide(self):
+        self.assertEqual(divide(10,2), 5)
+
+if __name__ == '__main__':
+    
+    unittest.main()            
