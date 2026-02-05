89f1b8cc12c2c19f807cb79e35086495570aa07b
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2026-02-05 12:04:45 +0530

    Delete Experiment_3 directory

--- Experiment_3/scientific-calculator/calculator.py
+++
@@ -1,7 +0,0 @@
-from basic_operations import add, subtract, multiply, divide
-from advanced_operations import square_root, power, sine, cosine, tangent
-
-def evaluate_expression():
-    return add(10, sine(30))
-
-
